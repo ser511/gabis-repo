128bd4ee9f9fb1ac60006f740379add3adb3e43d
Checho Quiroz <chechoquiroz@Chechos-MacBook-Air.local>
2022-01-05 16:07:44 -0600

    Adding GABIS

---
+++ GABIS.py
@@ -0,0 +1,10 @@
+#import the module
+import pyttsx3 
+
+#initialize pyttsx3 engine
+
+engine = pyttsx3.init()
+
+# convert to text to speech
+engine.say("Reyna is so cool, right OMG!!")
+engine.runAndWait()
